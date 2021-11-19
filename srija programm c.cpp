7c606f18215586f0f31fa33fed7bee1176b6cbf1
srijachalumuri <93577599+srijachalumuri@users.noreply.github.com>
2021-11-19 12:33:09 +0530

    Add files via upload

---
+++ srija programm c.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y,z;
+    printf("the number x:");
+    scanf("%d",&x);
+    printf("the number y:");
+    scanf("%d",&y);
+    z=x+y;
+    printf("the sum of two numbers is z: %d",z);
+    return(0);
+}
